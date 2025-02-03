10c3a40728530b8cdbe07c8feb14edaee1bf9308
angierecinos <rec23294@uvg.edu.gt>
2025-02-03 00:04:07 -0600

    SETUP
    Se especifican cómo funcionaran los ports así como ciertos registros importantes para poder desarrollar el contador.

--- PreLaboratorio01/PreLaboratorio01/main.asm
+++ PreLaboratorio01/PreLaboratorio01/main.asm
@@ -21,3 +21,21 @@ OUT		SPL, R16				// Configura spl = 0xFF -> r16
 LDI		R16, HIGH(RAMEND)		// Carga los bits altos (0x03)
 OUT		SPH, R16				// Configura sph = 0x03) -> r16
 
+// Configurar el microcontrolador
+SETUP:
+	//	Configurar pines de entrada y salida (DDRx, PORTx, PINx) 
+	//	Configurar PORTD como entrada con pull-up habilitado 
+	//	PORTD como entrada con pull-up habilitado
+	LDI		R16, 0x00	
+	OUT		DDRD, R16			// Setear puerto D como entrada (0 -> recibe)
+	LDI		R16, 0xFF	
+	OUT		PORTD, R16			// Habilitar pull-ups en puerto D
+
+	//	PORTB como salida inicialmente encendido
+	LDI		R16, 0xFF
+	OUT		DDRB, R16			// Setear puerto B como salida (1 -> no recibe)
+	LDI		R16, 0b0001			// Primer bit encendido
+	OUT		PORTB, R16			// Encender primer bit del puerto B
+	 
+	LDI		R17, 0xFF			// Variable para guardar el estado de botones
+	LDI		R19, 0x00			// Variable para contador
