b90a1a9defc0feb53aff3f1f57aef97d5e6ecf58
angierecinos <rec23294@uvg.edu.gt>
2025-02-13 21:54:59 -0600

    cambios
    cambios para prelab

--- PreLab03/PreLab03/main.asm
+++ PreLab03/PreLab03/main.asm
@@ -1,11 +1,6 @@
 ;
 ; PreLab03.asm
 ;
-;
-; PreLaboratorio03.asm
-;
-; 
-
 ; Universidad del Valle de Guatemala
 ; Departamento de Ingenieria Mecatronica, Electronica y Biomedica
 ; IE2023 Semestre 1 2025 
@@ -15,16 +10,19 @@
 ; Carnet : 23294
 ; 
 
-// --------------------- Encabezado ------------------------------- //
+// -------------------------------- Encabezado ------------------------------- //
 
 .include "M328PDEF.inc"				// Incluye definiciones del ATMega328
 .cseg								// Codigo en la flash
 
 .org	0x0000						// Donde inicia el programa
-	JMP	START					// Tiene que saltar para no ejecutar otros
+	JMP	START						// Tiene que saltar para no ejecutar otros
 
-.org	OVF0addr
-	JMP	ISR_TMR0
+.org	PCI0addr					// Dirección donde está el bit interrupción
+	JMP	ISR_PCINT0
+
+.org	
+.def	BOTONES = R21				// Guarda el estado de los botones
 
 START: 
 	// Configurar el SP en 0x03FF (al final de la SRAM) 
@@ -48,10 +46,11 @@ SETUP:
 	OUT		TCCR0B, R16					// Setear prescaler del TIMER 0 a 64
 	LDI		R16, 100					// Indicar desde donde inicia
 	OUT		TCNT0, R16					// Cargar valor inicial en TCNT0
-	RET
-	// Habilitar interrupciones con la máscara 
-	LDI		R16, (1<<TOIE0)				// Habilitar interrupciones del bit 0 para el 
-	STS		TIMSK0, R16
+	
+	
+	/*/ Habilitar interrupciones con la máscara 
+	LDI		R16, (1 << TOIE0)			// Habilitar interrupciones del bit 0 para el 
+	STS		TIMSK0, R16*/
 
 	//	Configurar PORTB como entrada con pull-up habilitado 
 	//	PORTB como entrada con pull-up habilitado
@@ -60,25 +59,62 @@ SETUP:
 	LDI		R16,	0xFF	
 	OUT		PORTB,	R16					// Habilitar pull-ups en puerto B
 
-	//	PORTD y PORTC como salida inicialmente encendido
-	LDI		R16,	0xFF			// Setear puerto C como salida (1 -> no recibe
+	//	 PORTC como salida 
+	LDI		R16,	0xFF				// Setear puerto C como salida (1 -> no recibe)
 	OUT		DDRC,	R16
-	LDI		R16,	0b0000			// Apagados	
-	OUT		PORTC,	R16				// Apagar bit del puerto C
+	LDI		R16,	0b0000				// Apagados	
+	OUT		PORTC,	R16					// Apagar bit del puerto C
+	
+	// Configuración de interrupción 
+	LDI		R16, (1 << PCINT0) | (1 << PCINT1)		// Se seleccionan los bits de la máscara (2)
+	STS		PCMSK0, R16								// Bits habilitados (PB0 y PB1) 			
 
-	SEI
+	LDI		R16, (1 << PCIE0)						// Habilita las interrupciones Pin-Change en PORTB
+	STS		PCICR, R16								// "Avisa" al registros PCICR que se habilitan en PORTB
+
+	SEI												// Se habilitan interrupciones globales
+
+	LDI		R17, 0x00								// Registro para contador
+
+LOOP:  
+     RJMP LOOP
+
 // Rutina de interrupción
-TMR0_ISR: 
-	PUSH	R16				// Para no perder el SREG
+ISR_PCINT0: 
+	PUSH	R16				// Para no perder el SREG lo mete a la pila
 	IN		R16, SREG		// Copia el valor de SREG 
 	PUSH	R16				// Lo saca
+	
+	// PB0 -> Incrementa | PB1 -> Decrementa	
+	IN		R18, PINB		// Revisa el estado de PINB
+	
+	SBRS	R18, PB0		// Si el bit 0 está set salta (por pull-up 1 -> suelto) 
+	CALL	INCREMENTAR1				// Si no está set incrementa (0 -> apachado) 
 
-	LDI		R16, 100		// Cuando se reinicia hay que explicar donde empieza
-	OUT		TCNT0, R16
-	SBI		TIFR0, TOV0		// Limpiar la bandera
-	INC		R20
+	SBRS	R18, PB1		// Revisa si el bit 1 está set (1 -> no apachado) 
+	CALL	DECREMENTAR1				// Si no está set decrementa (0 -> apachado)  
 
-	POP		R16
+	POP		R16				// Vuelve a meterle el valor anterior del SREG
 	OUT		SREG, R16
 	POP		R16
 	RETI
+
+// Sub-rutinas (no de interrupción) 
+INCREMENTAR1: 	
+	CPI		R17, 0x0F			// Compara el valor del contador 
+    BREQ	RESET_COUNTER1		// Si al comparar no es igual, salta a mostrarlo
+	INC		R17					// Incrementa el valor
+	OUT		PORTC, R17				// Muestra en el portD el valor
+	RET							// Vuelve al ciclo main a repetir
+
+DECREMENTAR1: 
+	CPI		R17, 0x00			// Si el contador llega a 0, reiniciar el contador
+	BREQ	RESET_COUNTER1		// Si es igual a 0 no hace nada y vuelve a main
+	DEC		R17					// R19 decrementará
+	OUT		PORTC, R17			// Muestra en el PORTD el cambio de contador
+	RET							// Regresa a main si ya decremento
+
+RESET_COUNTER1:
+    LDI		R17, 0x00			// Resetea el contador a 0
+	OUT		PORTC, R17
+	RET
