f7a9146e12483afc94b097cce8315f5f8ac1f59b
angierecinos <rec23294@uvg.edu.gt>
2025-02-24 16:50:12 -0600

    Creación de proyecto
    Se crea el documento para el proyecto

---
+++ Proyecto01/Proyecto01/main.asm
@@ -0,0 +1,17 @@
+;
+; Proyecto01.asm
+;
+;
+; Universidad del Valle de Guatemala
+; Departamento de Ingenieria Mecatronica, Electronica y Biomedica
+; IE2023 Semestre 1 2025 
+;
+; Created: 24/02/2025
+; Author : Angie Recinos
+; Carnet : 23294
+; Descripción: El código será un reloj con diferentes opciones
+
+// -------------------------------- Encabezado ------------------------------- //
+
+.include "M328PDEF.inc"				// Incluye definiciones del ATMega328
+.cseg								// Codigo en la flash
