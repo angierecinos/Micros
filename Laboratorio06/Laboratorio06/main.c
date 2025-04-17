3824a7fcb87caaa49c376b45be8f60dc99014486
angierecinos <rec23294@uvg.edu.gt>
2025-04-17 13:01:51 -0600

    prelab06
    se hace prelab06

--- Laboratorio06/Laboratorio06/main.c
+++ Laboratorio06/Laboratorio06/main.c
@@ -1,9 +1,9 @@
 /*
- * Clase_9_UART.c
+ * Laboratorio06.c
  *
- * Created: 9/04/2025 16:49:22
- * Author : Angie
- * Description: Clase para UART
+ * Created: 17/04/2025 11:48:01
+ * Author : Usuario
+ * Description: Se utiliza comunicación serial
  */ 
 
 //*******************************************
@@ -22,12 +22,13 @@ void writeChar(char caracter);
 int main(void)
 {
 	setup();
+	writeChar('A');
+	writeChar('N');
+	writeChar('G');
+	writeChar('I');
+	writeChar('E');
 	while (1)
 	{
-		writeChar('H');
-		writeChar('O');
-		writeChar('L');
-		writeChar('A');
 	}
 }
 
@@ -37,6 +38,8 @@ int main(void)
 void setup()
 {
 	cli();
+	DDRB = 0xFF;						// Se setea puerto B como salida 
+	PORTB = 0x00;						// Apaga la salida
 	initUART();
 	sei();
 }
@@ -47,30 +50,35 @@ void initUART()
 	DDRD |=  (1 << DDD1);
 	DDRD &= ~(1 << DDD0); 
 	
-	// 
+	// Se apaga (no utilizo doble velocidad) 
 	UCSR0A = 0; 
 	// Habilitar interrupts recibir, recepcion y transmision 
 	UCSR0B |= (1 << RXCIE0) | (1 << RXEN0) | (1 << TXEN0); 
-	// 
+	// Modo asíncrono y con paridad deshabilitada | Quiero 8 bits 
 	UCSR0C = (1 << UCSZ01) | (1 << UCSZ00); 
-	// UBRR = 103 -> 9600 @ 16MHz 
+	// Valor UBRR = 103 -> 9600 @ 16MHz 
 	UBRR0 = 103; 
 }
 
 void writeChar(char caracter)
 {
 	// Si no se hubiera esperado a que termine de trasladar, puede dar un error
-	uint8_t temporal = UCSR0A & (1 << UDRE0);
+	//uint8_t temporal = UCSR0A & (1 << UDRE0);
 	while ((UCSR0A & (1 << UDRE0)) == 0);
 	UDR0 = caracter; 
 	
 }
 //*******************************************
 // Interrupt routines
-
-// NON - Interrupt
 ISR(USART_RX_vect)
 {
 	char temporal = UDR0;
 	writeChar(temporal);
 }
+
+// NON - Interrupt
+
+
+
+
+
