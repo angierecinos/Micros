0c725ce394ca44d8e46053d58f6ba5a517538eab
angierecinos <rec23294@uvg.edu.gt>
2025-05-03 23:07:04 -0600

    avances proyecto
    se avanza con el proyecto para la comunicacion uart

--- Proyecto02/Proyecto02/main.c
+++ Proyecto02/Proyecto02/main.c
@@ -15,13 +15,14 @@
 #include "PWM0/PWM0.h"
 #include "PWM1/PWM1.h"
 #include "SERVO/SERVO.h"
+#include "UART/UART.h"
 
 uint8_t lectura_adc;
-uint16_t adc_value2;
+uint16_t option = 0;
 uint16_t adc_value;
 uint16_t pulse;
 uint16_t pulse2;
-uint8_t pin = 0;
+char texto = 0;
 uint8_t eleccion_adc =0;
 uint8_t counter = 0;
 uint8_t manual = 0;
@@ -30,7 +31,7 @@ uint8_t manual = 0;
 // Function prototypes
 void setup();
 void initADC();
-
+void processCoord(texto);
 //
 // Main Function
 int main(void)
@@ -57,8 +58,9 @@ void setup()
 	initPWM1B(non_invert, 8);					// No invertido y prescaler de 8
 
 	initADC();
+	initUART();
 	
-	DDRB  |= (1 << PORTB1) | (1 << PORTB2);	// En el timer1 pines PB1 | PB2
+	DDRB  |= (1 << PORTB1) | (1 << PORTB2) | (1 << PORTB0);		// En el timer1 pines PB1 | PB2 ??
 	DDRD  |= (1 << PORTD6) | (1 << PORTD5);
 	UCSR0B	= 0x00;								// Apaga serial
 	
@@ -79,23 +81,44 @@ void initADC()
 	ADCSRA	|= (1 << ADSC);						// Inicia con la conversi�n
 }
 
+void processCoord(texto)
+{
+	char* indice = texto;
+	uint8_t servo_index = 0;				// índice para coordenadas
+	uint16_t angulos[4] = {0};				// Se almacenarán las coordenadas
+
+	// Mientras sea menor a 4 y el índice sea diferente del nulo
+	while (servo_index < 4 && *indice != '\0') {
+		char temp[5] = {0};					// Buffer temporal para número
+		uint8_t i = 0;
+
+		while (*ptr != ',' && *ptr != '\0' && i < 4) {
+			temp[i++] = *ptr++;
+		}
+
+		angulos[servo_index++] = atoi(temp);
+
+		if (*ptr == ',') ptr++;  // saltar la coma
+	}
+
+	// Ahora puedes aplicar las funciones para mover servos
+	if (servo_index == 4) {
+		servo_positionA(angulos[0]);
+		servo_positionB(angulos[1]);
+		servo_position1A(angulos[2]);
+		servo_position1B(angulos[3]);
+	}
+}
+
 //
 // Interrupt routines
 ISR(ADC_vect)
 {
 	eleccion_adc = ADMUX & 0x0F;
-	lectura_adc = ADCH; 
-	/*if (eleccion_adc == 3)
+	lectura_adc = ADCH; 	
+	
+	if (manual == 1)
 	{
-		//adc_value = lectura_adc;
-		pin = 3; 
-	}else if (eleccion_adc == 2){
-		//adc_value2 = lectura_adc;
-		pin = 2;
-	}*/
-	
-	//pin = (ADMUX & 0x03); 
-	
 	
 	switch(eleccion_adc){
 		case 1:
@@ -128,6 +151,46 @@ ISR(ADC_vect)
 		default: 
 			break;  
 	}
-	
+	manual = 0; 
 	ADCSRA |= (1 << ADSC);								// Inicia conversi�n otra vez
+	}
+	else {
+		ADCSRA |= (1 << ADSC);	
+	}
+}
+
+ISR(USART_RX_vect)
+{
+	// Se mostrará menú para seleccionar modo 
+	char temporal = UDR0;
+	// Eco automático de dato ingresado
+	writeChar(temporal);
+	sendString(" \r\n");
+	
+	if (option == 0)
+	{
+		// Dependiendo de lo que se elija, será el modo
+		if(temporal == '1') {
+			
+			// Modo manual - Con la interrupción de ADC
+			sendString("\r\nModifique manualmente cada motor\r\n");
+			PORTB |= (1 << PORTB0);
+			manual = 1;
+			} else if (temporal == '2') {
+			// Modo UART
+			sendString("\r\nIngrese las posiciones de los cuatro motores en un angulo de 0° - 180° separados por comas.\r\n");
+			sendString("\r\nPor ejemplo: 30,60,90,180.\r\n");
+			PORTB &= ~(1 << PORTB0);
+			PORTD |= (1 << PORTD7);
+			option == 1; 
+		} else {
+		sendString("\r\nOpción no valida. Intente nuevamente.\r\n");
+		}
+	} else if (option == 1)
+		{
+			sendString("\r\nCoordenadas: \r\n");
+			
+			
+		}
+	
 }
